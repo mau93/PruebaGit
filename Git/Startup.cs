ec8fed57ac013a752da6d616e62b8d7bdf5e8207
mau93 <isc.mauriciobg@gmail.com>
2016-10-03 00:00:38 -0500

    confirmados

---
+++ Git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Git.Startup))]
+namespace Git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
